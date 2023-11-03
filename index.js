22369fcbde7f4c5a5abda34ea6515641b331b1c0
NicuV23 <v.nicu213@gmail.com>
2023-11-03 13:35:36 +0200

    final modify

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
 const buttons = document.querySelectorAll('.button');
 const messageElement = document.getElementById('message');
+const tryAgainButton = document.getElementById('try-again');
 let messageDisplayed = false;
 let winningButtonIndex = getRandomNumber();
+let clickedButtonIndex;
 
 function getRandomNumber() {
   return Math.floor(Math.random() * buttons.length);
 }
 
 function handleButtonClick(event) {
-  const buttonIndex = Array.from(buttons).indexOf(event.target);
-
   if (!messageDisplayed) {
-    if (buttonIndex === winningButtonIndex) {
+    buttons.forEach(button => {
+      button.style.backgroundColor = 'red';
+      button.disabled = true;
+    });
+
+    clickedButtonIndex = Array.from(buttons).indexOf(event.target);
+
+    if (clickedButtonIndex === winningButtonIndex) {
       messageElement.textContent = 'Winner';
     } else {
       messageElement.textContent = 'Try again';
     }
+
+    tryAgainButton.style.display = 'inline';
     messageDisplayed = true;
-  } else {
-    messageElement.textContent = '';
-    messageDisplayed = false;
+
+    buttons[winningButtonIndex].style.backgroundColor = 'green';
   }
 }
 
 function handleTryAgainClick() {
+  buttons.forEach(button => {
+    button.style.backgroundColor = '';
+    button.disabled = false;
+  });
+
   messageElement.textContent = '';
   messageDisplayed = false;
   winningButtonIndex = getRandomNumber();
+  tryAgainButton.style.display = 'none';
 }
 
 buttons.forEach(button => {
   button.addEventListener('click', handleButtonClick);
 });
 
-const tryAgainButton = document.getElementById('try-again');
 tryAgainButton.addEventListener('click', handleTryAgainClick);
